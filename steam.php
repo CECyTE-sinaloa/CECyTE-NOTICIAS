4f8cc22b11734a28c23a66ef8279ccf0c41035c6
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-17 22:05:51 -0500

    Add files via upload

---
+++ steam.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" href="CE.png">
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CECyTE Noticias</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style type="text/css">
+    body{
+    	background: white;
+    }
+    	img{
+        border-radius: 5px;
+        width: 200px;
+      }
+      .galeria{
+        width: 420px;
+        height: 500px;
+
+      }
+    </style>
+</head>
+<body>
+ 	<div class="card-group">
+    <div class="card">
+     <div class="card-body">
+       <a href="index.php"><button id="qwe" style="font-size:40px;" type="button" class="close" data-dismiss="modal" aria-label="Close">
+       <span aria-hidden="true">&times;</span>
+       </button></a>
+       <h5 class="card-title"><b><center>Mujeres Lideres en STEAM</center></b></h5>
+       <p class="card-text">CECyTE Sinaloa plantel la Cruz de nueva cuenta entra a la convocatoria de mujeres líderes en STEAM.
+¿Que es esto?😱Mujeres Lideres en STEAM es un programa único de mentoría en áreas de Ciencias, Tecnología, Ingeniería y Matemáticas (STEM) para Mujeres Latinas. El programa fue creado por la Lic. MBA Rebeca Vargas en base a su experiencia profesional; para contribuir al EMPODERAMIENTO de jóvenes mujeres y para acercarlas al mundo de STEM para inculcar en ellas el deseo por iniciar una carrera profesional en alguna de estas áreas. El programa es exitoso en preparar a las futuras generaciones de # MujeresLíderesEnSTEAM
+#EmpoderamientoCECyTE #SiSePuede 👌👌</p>
+       <ul class="galeria">    
+        <li><a href="#img1"><img src="steam/1.jpg" class="card-img-top"></a></li>    
+        <li><a href="#img2"><img src="steam/2.jpg" class="card-img-top"></a></li>    
+        <li><a href="#img3"><img src="steam/3.jpg" class="card-img-top"></a></li>    
+        <li><a href="#img4"><img src="steam/4.jpg" class="card-img-top"></a></li>
+       </ul> 
+     </div>
+    </div>
+    <div class="modal" id="img1"> 		 		
+    <div class="imagen"> 			
+    <a href="#img42"><b>&#60;</b></a> 			
+    <a href="#img1"><img src="steam/1.jpg"></a> 			
+    <a href="#img2"><b>></b></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div> 	 	
+    <div class="modal" id="img2"> 		 		
+    <div class="imagen"> 			
+    <a href="#img1">&#60;</a> 			
+    <a href="#img2"><img src="steam/2.jpg"></a> 			
+    <a href="#img3"><b>></b></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div> 	 	
+    <div class="modal" id="img3"> 				
+    <div class="imagen"> 			
+    <a href="#img2">&#60;</a> 			
+    <a href="#img3"><img src="steam/3.jpg"></a> 			
+    <a href="#img4">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div> 	 	
+    <div class="modal" id="img4"> 		 		
+    <div class="imagen"> 			
+    <a href="#img3">&#60;</a> 			
+    <a href="#img4"><img src="steam/4.jpg"></a> 			
+    <a href="#img1">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div> 
+</body>
+</html>
