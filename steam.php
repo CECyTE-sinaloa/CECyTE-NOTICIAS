b43ae87b7adcbb9e5ccb102a59ad093ffd308b49
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-17 22:33:38 -0500

    Update steam.php

--- steam.php
+++ steam.php
@@ -1,46 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="shortcut icon" href="CE.png">
 <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>CECyTE Noticias</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="CE.png">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <style type="text/css">
     body{
     	background: white;
     }
     	img{
         border-radius: 5px;
-        width: 200px;
       }
       .galeria{
-        width: 420px;
-        height: 500px;
-
+        width: 100%;
+        height: 100%;
       }
+     .card{
+       width: 100%;
+       height: 100%;
+       border-color: transparent;
+       border-bottom-color: transparent;
+     }
     </style>
-</head>
+ </head>
 <body>
- 	<div class="card-group">
+<div class="container-fluid">
     <div class="card">
      <div class="card-body">
        <a href="index.php"><button id="qwe" style="font-size:40px;" type="button" class="close" data-dismiss="modal" aria-label="Close">
        <span aria-hidden="true">&times;</span>
        </button></a>
-       <h5 class="card-title"><b><center>Mujeres Lideres en STEAM</center></b></h5>
-       <p class="card-text">CECyTE Sinaloa plantel la Cruz de nueva cuenta entra a la convocatoria de mujeres líderes en STEAM.
+       <h5 class="card-title" style="font-size: 1.5em;"><b><center>Mujeres Lideres en STEAM</center></b></h5>
+       <h6 class="card-text" style="font-size: 1em;"><center>CECyTE Sinaloa plantel la Cruz de nueva cuenta entra a la convocatoria de mujeres líderes en STEAM.
 ¿Que es esto?😱Mujeres Lideres en STEAM es un programa único de mentoría en áreas de Ciencias, Tecnología, Ingeniería y Matemáticas (STEM) para Mujeres Latinas. El programa fue creado por la Lic. MBA Rebeca Vargas en base a su experiencia profesional; para contribuir al EMPODERAMIENTO de jóvenes mujeres y para acercarlas al mundo de STEM para inculcar en ellas el deseo por iniciar una carrera profesional en alguna de estas áreas. El programa es exitoso en preparar a las futuras generaciones de # MujeresLíderesEnSTEAM
-#EmpoderamientoCECyTE #SiSePuede 👌👌</p>
-       <ul class="galeria">    
-        <li><a href="#img1"><img src="steam/1.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img2"><img src="steam/2.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img3"><img src="steam/3.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img4"><img src="steam/4.jpg" class="card-img-top"></a></li>
+#EmpoderamientoCECyTE #SiSePuede 👌👌</center></h6>
+      <center>
+      <ul class="galeria">    
+        <li><a href="#img1"><img src="steam/1.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img2"><img src="steam/2.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img3"><img src="steam/3.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img4"><img src="steam/4.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img5"><img src="steam/5.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img6"><img src="steam/6.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img7"><img src="steam/7.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img8"><img src="steam/8.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img9"><img src="steam/9.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img10"><img src="steam/10.jpg" class="img-fluid" alt="Responsive image"></a></li> 
        </ul> 
+      </center>
      </div>
-    </div>
+   </div>
+ </div>
     <div class="modal" id="img1"> 		 		
     <div class="imagen"> 			
     <a href="#img42"><b>&#60;</b></a> 			
@@ -69,9 +82,57 @@
     <div class="imagen"> 			
     <a href="#img3">&#60;</a> 			
     <a href="#img4"><img src="steam/4.jpg"></a> 			
-    <a href="#img1">></a> 		
+    <a href="#img5">></a> 		
     </div> 		
     <a class="cerrar" href=""><center><b>x</b></center></a> 	
     </div> 
-</body>
-</html>
+    <div class="modal" id="img5"> 		 		
+    <div class="imagen"> 			
+    <a href="#img4">&#60;</a> 			
+    <a href="#img5"><img src="steam/5.jpg"></a> 			
+    <a href="#img6">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div>
+    <div class="modal" id="img6"> 		 		
+    <div class="imagen"> 			
+    <a href="#img5">&#60;</a> 			
+    <a href="#img6"><img src="steam/6.jpg"></a> 			
+    <a href="#img7">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div>
+    <div class="modal" id="img7"> 		 		
+    <div class="imagen"> 			
+    <a href="#img6">&#60;</a> 			
+    <a href="#img7"><img src="steam/7.jpg"></a> 			
+    <a href="#img8">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div> 
+    <div class="modal" id="img8"> 		 		
+    <div class="imagen"> 			
+    <a href="#img7">&#60;</a> 			
+    <a href="#img8"><img src="steam/8.jpg"></a> 			
+    <a href="#img9">></a> 		
+    </div> 		
+    <a class="cerrar" href=""><center><b>x</b></center></a> 	
+    </div>
+    <div class="modal" id="img9">         
+    <div class="imagen">      
+    <a href="#img8">&#60;</a>       
+    <a href="#img9"><img src="steam/9.jpg"></a>       
+    <a href="#img10">></a>     
+    </div>    
+    <a class="cerrar" href=""><center><b>x</b></center></a>   
+    </div>
+    <div class="modal" id="img10">         
+    <div class="imagen">      
+    <a href="#img9">&#60;</a>       
+    <a href="#img10"><img src="steam/10.jpg"></a>       
+    <a href="#img1">></a>     
+    </div>    
+    <a class="cerrar" href=""><center><b>x</b></center></a>   
+    </div>
+    </body>
+    </html>       
