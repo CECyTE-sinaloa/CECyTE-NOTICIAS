46fce2d844e8b8f575b27cc714189038383dc2f9
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-17 22:29:42 -0500

    Update festival-arte-cultura.php

--- festival-arte-cultura.php
+++ festival-arte-cultura.php
@@ -1,58 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="shortcut icon" href="CE.png">
 <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>CECyTE Noticias</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="CE.png">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <style type="text/css">
     body{
     	background: white;
     }
     	img{
         border-radius: 5px;
-        width: 200px;
       }
       .galeria{
-        width: 420px;
-        height: 500px;
-
+        width: 100%;
+        height: 100%;
       }
+     .card{
+       width: 100%;
+       height: 100%;
+       border-color: transparent;
+       border-bottom-color: transparent;
+     }
     </style>
-</head>
+ </head>
 <body>
- 	<div class="card-group">
+<div class="container-fluid">
     <div class="card">
      <div class="card-body">
        <a href="index.php"><button id="qwe" style="font-size:40px;" type="button" class="close" data-dismiss="modal" aria-label="Close">
        <span aria-hidden="true">&times;</span>
        </button></a>
-       <h5 class="card-title"><b><center>Festival estatal de arte y cultura de los CECyTES</center></b></h5>
-       <p class="card-text">Felicitamos a todos los alumnos participantes de nuestro plantel #CecyteLaCruz por su excelente participación en el festival estatal de arte y cultura de los CECyTES llevado a cabo en instalacuones de CECADET en Ciudad Educadora Culiacan #OrgulloCecyte #CecyteLaCruz #YoSoyCecyte</p>
-       <ul class="galeria">    
-        <li><a href="#img1"><img src="a-c/1.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img2"><img src="a-c/2.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img3"><img src="a-c/3.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img4"><img src="a-c/4.jpg" class="card-img-top"></a></li>
-        <li><a href="#img5"><img src="a-c/5.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img6"><img src="a-c/6.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img7"><img src="a-c/7.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img8"><img src="a-c/8.jpg" class="card-img-top"></a></li> 
-        <li><a href="#img9"><img src="a-c/9.jpg" class="card-img-top"></a></li> 
-        <li><a href="#img10"><img src="a-c/10.jpg" class="card-img-top"></a></li>  
-        <li><a href="#img11"><img src="a-c/11.jpg" class="card-img-top"></a></li>
-        <li><a href="#img12"><img src="a-c/12.jpg" class="card-img-top"></a></li>
-        <li><a href="#img13"><img src="a-c/13.jpg" class="card-img-top"></a></li>
-        <li><a href="#img14"><img src="a-c/14.jpg" class="card-img-top"></a></li>
-        <li><a href="#img15"><img src="a-c/15.jpg" class="card-img-top"></a></li>
-        <li><a href="#img16"><img src="a-c/16.jpg" class="card-img-top"></a></li>
-        <li><a href="#img17"><img src="a-c/17.jpg" class="card-img-top"></a></li>
-        <li><a href="#img18"><img src="a-c/18.jpg" class="card-img-top"></a></li>
+       <h5 class="card-title" style="font-size: 1.5em;"><b><center>Festival estatal de arte y cultura de los CECyTES</center></b></h5>
+       <h6 class="card-text" style="font-size: 1em;"><center>Felicitamos a todos los alumnos participantes de nuestro plantel #CecyteLaCruz por su excelente participación en el festival estatal de arte y cultura de los CECyTES llevado a cabo en instalacuones de CECADET en Ciudad Educadora Culiacan #OrgulloCecyte #CecyteLaCruz #YoSoyCecyte</center></h6>
+      <center>
+      <ul class="galeria">    
+        <li><a href="#img1"><img src="a-c/1.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img2"><img src="a-c/2.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img3"><img src="a-c/3.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img4"><img src="a-c/4.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img5"><img src="a-c/5.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img6"><img src="a-c/6.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img7"><img src="a-c/7.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img8"><img src="a-c/8.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img9"><img src="a-c/9.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img10"><img src="a-c/10.jpg" class="img-fluid" alt="Responsive image"></a></li>  
+        <li><a href="#img11"><img src="a-c/11.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img12"><img src="a-c/12.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img13"><img src="a-c/13.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img14"><img src="a-c/14.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img15"><img src="a-c/15.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img16"><img src="a-c/16.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img17"><img src="a-c/17.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img18"><img src="a-c/18.jpg" class="img-fluid" alt="Responsive image"></a></li>
        </ul> 
+      </center>
      </div>
-    </div>
+   </div>
+ </div>
     <div class="modal" id="img1"> 		 		
     <div class="imagen"> 			
     <a href="#img42"><b>&#60;</b></a> 			
@@ -197,5 +204,5 @@
     </div>    
     <a class="cerrar" href=""><center><b>x</b></center></a>   
     </div>
-</body>
-</html>
+    </body>
+    </html>       
