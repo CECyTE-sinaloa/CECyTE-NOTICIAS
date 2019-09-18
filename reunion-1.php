2113499612fea3cedbed51ac8cf556cf39bb6359
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-17 22:38:37 -0500

    Update reunion-1.php

--- reunion-1.php
+++ reunion-1.php
@@ -1,45 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="shortcut icon" href="CE.png">
 <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>CECyTE Noticias</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="CE.png">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <style type="text/css">
     body{
     	background: white;
     }
     	img{
         border-radius: 5px;
-        width: 200px;
       }
       .galeria{
-        width: 420px;
-        height: 500px;
-
+        width: 100%;
+        height: 100%;
       }
+     .card{
+       width: 100%;
+       height: 100%;
+       border-color: transparent;
+       border-bottom-color: transparent;
+     }
     </style>
-</head>
+ </head>
 <body>
- 	<div class="card-group">
+<div class="container-fluid">
     <div class="card">
      <div class="card-body">
        <a href="index.php"><button id="qwe" style="font-size:40px;" type="button" class="close" data-dismiss="modal" aria-label="Close">
        <span aria-hidden="true">&times;</span>
        </button></a>
-       <h5 class="card-title"><b><center>La primer reunión con padres de familia de alumnos de 1er año</center></b></h5>
-       <p class="card-text">Hoy 10 de Septiembre se llevo a cabo la primer reunión con padres de familia de alumnos de 1er año con el fin de darles a conocer mas información sobre nuestro plantel, temas de evaluación y lo mas importante acuerdos tomados por padres y por la institución.
-#YaEresCECyTE #FuerzaCECyTE #AunTenemosUnLugarParaTi</p>
-       <ul class="galeria">    
-        <li><a href="#img1"><img src="reunion-1/1.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img2"><img src="reunion-1/2.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img3"><img src="reunion-1/3.jpg" class="card-img-top"></a></li>    
-        <li><a href="#img4"><img src="reunion-1/4.jpg" class="card-img-top"></a></li>
+       <h5 class="card-title" style="font-size: 1.5em;"><b><center>La primer reunión con padres de familia de alumnos de 1er año</center></b></h5>
+       <h6 class="card-text" style="font-size: 1em;"><center>Hoy 10 de Septiembre se llevo a cabo la primer reunión con padres de familia de alumnos de 1er año con el fin de darles a conocer mas información sobre nuestro plantel, temas de evaluación y lo mas importante acuerdos tomados por padres y por la institución.
+#YaEresCECyTE #FuerzaCECyTE #AunTenemosUnLugarParaTi</center></h6>
+      <center>
+      <ul class="galeria">    
+        <li><a href="#img1"><img src="reunion-1/1.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img2"><img src="reunion-1/2.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img3"><img src="reunion-1/3.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img4"><img src="reunion-1/4.jpg" class="img-fluid" alt="Responsive image"></a></li>
+        <li><a href="#img5"><img src="reunion-1/5.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img6"><img src="reunion-1/6.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img7"><img src="reunion-1/7.jpg" class="img-fluid" alt="Responsive image"></a></li>    
+        <li><a href="#img8"><img src="reunion-1/8.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img9"><img src="reunion-1/9.jpg" class="img-fluid" alt="Responsive image"></a></li> 
+        <li><a href="#img10"><img src="reunion-1/10.jpg" class="img-fluid" alt="Responsive image"></a></li> 
        </ul> 
+      </center>
      </div>
-    </div>
+   </div>
+ </div>
     <div class="modal" id="img1"> 		 		
     <div class="imagen"> 			
     <a href="#img42"><b>&#60;</b></a> 			
@@ -120,5 +133,5 @@
     </div>    
     <a class="cerrar" href=""><center><b>x</b></center></a>   
     </div>
-</body>
-</html>
+    </body>
+    </html>       
