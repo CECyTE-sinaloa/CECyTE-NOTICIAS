fa4b14c48f40c6a09af36b858b2782bb36549f0d
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-17 22:13:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CECyTE_Noticias</title>
+    <title>CECyTE Noticias</title>
     <link rel="shortcut icon" href="CE.png"/>
     <link rel="stylesheet" href="css/bootstrap.css"/>
     <link href="css/bootstrap-grid.css" rel="stylesheet" id="bootstrap-css"/>
@@ -13,9 +13,6 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/jquery-1.11.1.js"></script>
-    <script type="text/javascript">
-      alert("Por favor de volver más tarde, la página está en mantenimiento");
-    </script>
     <style type="text/css">
       body{
         background: rgba(0, 0, 100, 0.3);
@@ -47,6 +44,311 @@
     </style>
  </head>
 <body class="body">
- <center><h1>La Página está en Mantenimiento</h1></center>
+ <div class="pos-f-t">
+                    <div class="collapse" id="navbarToggleExternalContent">
+                      <div class="bg-dark p-4">
+                        <h5 class="text-white h4">Men&uacute;</h5>
+                        <span class="text-muted">El mejor sitio de noticias</span>
+                        <ul class="navbar-nav mr-auto">
+                          <li class="nav-item active">
+                            <a class="nav-link" href="index.html"><span style="color:#eee;">Inicio</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="index2.8.html"><span style="color:#eee;"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Correo.html"><span style="color:#eee;"></span></a>
+                        </li>
+                        </ul>
+                      </div>
+                    </div>
+  
+                    <nav class="navbar navbar-light bg-light">
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <img src="CE.png" width="70px;" height="50px;">
+                    </nav>
+                  </div><br>
+              <div class="jumbotron" id="jumbo">
+               <center><h1 class="titu">NOTICIAS CECyTE LA CRUZ</h1></center>
+              </div>
+              <div class="container-fluid" id="qw">
+            <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Festival estatal de arte y cultura de los CECyTES</h5>
+                    <p class="card-text">Felicitamos a todos los alumnos participantes de nuestro plantel #CecyteLaCruz por su excelente participación...</p>
+                    <a href="festival-arte-cultura.php"><button type="button" class="btn btn-outline-info" >
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                    <img src="a-c/17.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Mujeres Lideres en STEAM</h5>
+                    <p class="card-text">CECyTE Sinaloa plantel la Cruz de nueva cuenta entra a la convocatoria de mujeres líderes en STEAM.¿Que es esto?😱..</p>
+                    <a href="steam.php"><button type="button" class="btn btn-outline-info" >
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                    <img src="steam/3.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">La primer reunión con padres de familia de alumnos de 1er año</h5>
+                    <p class="card-text">Hoy 10 de Septiembre se llevo a cabo la primer reunión con padres de familia de alumnos de 1er año con el fin de darles...</p>
+                    <a href="reunion-1.php"><button type="button" class="btn btn-outline-info">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                    <img src="reunion-1/3.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+              </div>
+          </div>
+             <div class="container-fluid" id="qw">
+            <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">CLUB ECOAMIGOS</h5>
+                    <p class="card-text">El Club Ecoamigos realizaron labores de limpieza en el Plantel...</p>
+                    <a href="ecoamigos.html"><button type="button" class="btn btn-outline-info">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="ecoamigos/eco1.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">"Bienvenido a CECyTE Sinaloa</h5>
+                    <p class="card-text">#CECyTE Muévete, La mejor experiencia de preparatoria</p>
+                    <a href="index4.1.html"><button type="button" class="btn btn-outline-info" >
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <video controls>
+                       <source src="VID-2.mp4" type="video/mp4">
+                     </video>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">#CECYTE#LaCruz</h5>
+                    <p class="card-text">Una entrevista a los alumnos de cecyte la cruz, al aver optenido el primer y segundo lugar en el primer campamento tecnologico...</p>
+                    <a href="index4.2.html"><button type="button" class="btn btn-outline-info">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <video controls>
+                       <source src="VID-3.mp4" type="video/mp4">
+                     </video>
+                  </div>
+                </div>
+              </div>
+          </div>
+              <div class="container-fluid" id="qw">
+               <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">CECyTE Sinaloa Plantel La Cruz</h5>
+                    <p class="card-text">#CECyTE hoy tuvimos la presencia de la MC Elizabeth Osuna Directora del plantel culiacan en representacion de nuestra Directora general de CECyTE Sinaloa #AglaeMontoya...</p>
+                    <a href="index4.3.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <video controls>
+                       <source src="VID-4.mp4" type="video/mp4">
+                     </video>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">CECyTE ABP CAMP TECHNOLOGY</h5>
+                    <p class="card-text">Participación activa de los jóvenes en el campamento tecnologico...</p>
+                    <a href="index4.4.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <video controls>
+                       <source src="VID-5.mp4" type="video/mp4">
+                     </video>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">CECyTE Sinaloa Plantel La Cruz</h5>
+                    <p class="card-text">#CECyTE hoy tuvimos la presencia de la MC Elizabeth Osuna Directora del plantel culiacan en representacion de nuestra Directora general de CECyTE Sinaloa #AglaeMontoya...</p>
+                    <a href="index4.3.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <video controls>
+                       <source src="VID-4.mp4" type="video/mp4">
+                     </video>
+                  </div>
+                </div>
+              </div>
+          </div>
+        <div class="container-fluid" id="qw">
+            <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Actividades Cecyte</span>
+                    <p class="card-text">Se realizó la segunda sesión del Programa Mindfulness en la Escuela”.. misma que se realizó para docente por parte de la Educación Neuroemocional.. dirigida por la Dra. Caliope Frikas.</p>
+                    <a href="index2.9.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="log1.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                  <div class="card">
+                    <div class="card-body">
+                      <span class="card-title">Actividades Cecyte</span>
+                      <p class="card-text">Hoy 8 de marzo Día Internacional de la Mujer, brindamos una plática al alumnado de CECyTE, donde resaltamos la Lucha por la Igualdad de Derechos entre Mujeres y Hombres...</p>
+                       <a href="index3.0.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                      <img src="log7.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                    </div>
+                  </div>
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Evento del difunsor municipal</span>
+                    <p class="card-text">Felicitamos a nuestros Alumnos de los grupos 201 kimberlyn Pacheco Reyes, Selena García Castro, Jassiel Salas, Emmanuel Barraza Herrera, Jimena Inzunza, 402 Brayan Rodríguez López...
+                         </p>
+                    <a href="index3.1.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                    <img src="log8.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="container-fluid" id="qw">
+              <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Visita de la Lic. Lourdes Palazuelos</span>
+                    <p class="card-text">Cecyte Sinaloa Plantel La Cruz <br>
+                          28 de febrero a las 20:56 · <br> Hoy 28 de febrero se tuvo la visita de la Lic. Lourdes Palazuelos, encargada del departamento de vinculación de CECyTE Sinaloa...</p>
+                    <a href="index3.3.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Visita de la Lic. Lourdes Palazuelos/log.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                  <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Tomato Valley</span>
+                    <p class="card-text">Los Alumnos de 3er y 2do grado asisitieron a las ciudad de Culiacán a la empresa Tomato Valley en compañía de los expertos en programación la Profesora Ciria Diana Meza Favela, Uciel Iván Ontiveros Acosta y Miguel León...</p>
+                    <a href="index3.4.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Tomato/log3.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div>
+                </div>
+                  <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Día de la Bandera</span>
+                    <p class="card-text">La escolta oficial y banderín participaron en el desfile conmemorativo del Día de la Bandera convocada por el H. Ayuntamiento de Elota hoy 24 de Febrero del año en curso, Cecyte La Cruz le agradece a los jóvenes que participaron...</p>
+                    <a href="index3.5.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Día de la Bandera/log2.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div> 
+                </div>
+               </div>
+           </div>
+               <div class="container-fluid" id="qw">
+                <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Eventos del STEAM</span>
+                    <p class="card-text">Alumnas del Plantel Cristina Osuna Lizarraga y Rosaisel Sánchez Cruz, asistieron el día de hoy 9 de febrero del 2019 a la ciudad de Culiacán para informarse sobre el programa de STEM...</p>
+                    <a href="index3.6.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="log9.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div><br>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Reunión del cuerpo académico</span>
+                    <p class="card-text">El día de hoy se llevó acabo la primer reunión del cuerpo académico colegiado del Semestre B 2019 con la presencia de Dirección Académica de Oficinas Generales La Directora Mar de Jesús Heiras Palazuelos, Ing. Jonathan Jassan Rodríguez...</p>
+                    <a href="index3.2.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Reunión del cuerpo académico/log2.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div> 
+                </div> 
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Beca Transporte</span>
+                    <p class="card-text">Sugey Medina Real alumna de nuestro plantel y beneficiaria de la beca TRANSPORTE, recibio el cheque...</p>
+                    <a href="index3.7.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Transporte/log1.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div> 
+                </div>
+                </div> 
+              </div> 
+              <div class="container-fluid" id="qw"> 
+                <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Desfile de Luz</span>
+                    <p class="card-text">“La noche de hoy 15 de Diciembre del año en curso, Plantel Cecyte La Cruz participó en el desfile de la Luz convocada por el H. Ayuntamiento de Elota, le agradecemos a la Profra. Araceli, Profra. Ciria y al Profr. Juan Carlos por esta...</p>
+                    <a href="index3.8.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Desfile de Luz/log3.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div> 
+                </div> 
+                <div class="card">
+                  <div class="card-body">
+                    <span class="card-title">Cierre del curso de programación</span>
+                    <p class="card-text">El dia viernes 14 de Diciembre del presente año, se da el cierre del curso de programación en CECyTE La Cruz.<br>
+                    Nuestro plantel...</p>
+                    <a href="index3.9.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                     <img src="Cierre del curso de programación/log2.jpg" class="card-img-top" alt="logo" width="150px" height="300px">
+                  </div> 
+                </div> 
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cecyte ABP CAMP TECHNOLOGY</h5>
+                    <p class="card-text">CECyTE de La cruz consigue el primer y segundo lugar en el primer campamento tecnologico</p>
+                    <a href="index4.0.html"><button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModalCenter">
+                      Mas informacion
+                    </button></a>
+                    <br><br>
+                  </div>
+                </div>
+              </div> 
+              </div> 
+                <br>
+                <div>
+                    <img src="ggg.png" class="img-fuid" style="width: 100%;" alt="Responsive image">
+              </div>
+              <div class="footer">
+              </div>
 </body>
 </html>
