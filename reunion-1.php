0a0ccfe7d524f1ade7f230ea57ceb828b375b705
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-19 16:44:12 -0500

    Update reunion-1.php

--- reunion-1.php
+++ reunion-1.php
@@ -133,5 +133,8 @@
     </div>    
     <a class="cerrar" href=""><center><b>x</b></center></a>   
     </div>
+      <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v4.0"></script>
+ <div class="fb-comments" data-href="http://cecyte-noticias.herokuapp.com/reunion-1.php" data-width="" data-numposts="15"></div>
     </body>
     </html>       
