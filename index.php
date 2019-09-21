56231478056a667196fa7a2790abd8b2bb59097d
CECyTE-sinaloa <54907752+CECyTE-sinaloa@users.noreply.github.com>
2019-09-21 00:47:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                   <div class="card-body">
                     <h5 class="card-title">Festival estatal de arte y cultura de los CECyTES</h5>
                     <p class="card-text">Felicitamos a todos los alumnos participantes de nuestro plantel #CecyteLaCruz por su excelente participación...</p>
-                    <br><br>
+                    <br>
                    <a href="festival-arte-cultura.php"><img src="a-c/17.jpg" class="card-img-top" alt="logo" width="150px" height="220px"></a> 
                     <br><br>
                    <div id="fb-root"></div>
@@ -104,7 +104,7 @@
                   <div class="card-body">
                     <h5 class="card-title">Mujeres Lideres en STEAM</h5>
                     <p class="card-text">CECyTE Sinaloa plantel la Cruz de nueva cuenta entra a la convocatoria de mujeres líderes en STEAM.¿Que es esto?😱..</p>
-                    <br><br>
+                    <br>
                     <a href="steam.php"><img src="steam/3.jpg" class="card-img-top" alt="logo" width="150px" height="220px"></a>
                    <br><br>
                    <div id="fb-root"></div>
@@ -116,7 +116,7 @@
                   <div class="card-body">
                     <h5 class="card-title">La primer reunión con padres de familia de alumnos de 1er año</h5>
                     <p class="card-text">Hoy 10 de Septiembre se llevo a cabo la primer reunión con padres de familia de alumnos de 1er año con el fin de darles...</p>
-                    <br><br>
+                    <br>
                     <a href="reunion-1.php"><img src="reunion-1/3.jpg" class="card-img-top" alt="logo" width="150px" height="220px"></a>
                    <br><br>
                    <div id="fb-root"></div>
@@ -128,15 +128,16 @@
           </div>
              <div class="container-fluid" id="qw">
             <div class="card-group">
-                <div class="card">
+                <div class="card" id="#card4">
                   <div class="card-body">
                     <h5 class="card-title">CLUB ECOAMIGOS</h5>
                     <p class="card-text">El Club Ecoamigos realizaron labores de limpieza en el Plantel...</p>
-                    <a href="ecoamigos.html"><button type="button" class="btn btn-outline-info">
-                      Mas informacion
-                    </button></a>
+                   <br>
+                    <a href="ecoamigos.html"><img src="ecoamigos/eco1.jpg" class="card-img-top" alt="logo" width="150px" height="220px"></a>
                     <br><br>
-                     <img src="ecoamigos/eco1.jpg" class="card-img-top" alt="logo" width="150px" height="220px">
+                     <div id="fb-root"></div>
+                     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v4.0"></script>
+                   <div class="fb-like" data-href="http://cecyte-noticias.herokuapp.com/#card4" data-width="" data-layout="button" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
                   </div>
                 </div>
                 <div class="card">
